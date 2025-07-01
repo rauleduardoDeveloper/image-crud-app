dbe6d782089000efdfbb172b25ec3e93d9763cbc
rauleduardoDeveloper <rauleduardo.developer@gmail.com>
2025-07-02 01:43:52 +0500

    Feature-frontend: Added Get Images Feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,12 +19,12 @@ type Image = {
 
 function App() {
 const [images, setImages] = useState<Image[]>([]);
- 
+const [searchQuery,setSearchQuery]=useState<String>("")
 
  
 
   const fileInputRef=useRef<HTMLInputElement>(null)
-  
+  const debounceRef = useRef<NodeJS.Timeout | null>(null);
 const openFileSelector=()=>{
  
 fileInputRef.current?.click();
@@ -32,7 +32,21 @@ fileInputRef.current?.click();
  
 }
 
- 
+const getImages=async(searchQuery?:String)=>{
+  try{
+
+    const response=await axios.get(`${BASE_URL}/api/images`,{
+      params: {
+        search: searchQuery || "",
+      },
+    })
+    setImages(response?.data?.images)
+    
+
+  }catch(error){
+   toast.error("Something Went Wrong")
+  }
+}
 
   const handleSubmit = async(e: React.ChangeEvent<HTMLInputElement>) => {
     try{
@@ -65,9 +79,18 @@ fileInputRef.current?.click();
   };
 
   
-   
-  
- 
+   useEffect(() => {
+    if (debounceRef.current) {
+      clearTimeout(debounceRef.current);
+    }
+
+    debounceRef.current = setTimeout(() => {
+      getImages(searchQuery);
+    }, 500); 
+  }, [searchQuery]);
+useEffect(()=>{
+  getImages()
+},[])
   return (
  <div className='w-100 min-vh-100 d-flex flex-column px-4'>
   <Toaster position="bottom-right" reverseOrder={false} />
@@ -76,7 +99,7 @@ fileInputRef.current?.click();
     <div className='col-12 col-lg-3 col-md-8'>
       <input
         type='text'
-     
+        onChange={(e) => setSearchQuery(e.target.value)}
         placeholder='Search images...'
         className='form-control w-100'
       />
@@ -97,7 +120,51 @@ fileInputRef.current?.click();
  
 
 
-   
+    <div className="row g-2 my-5">
+{images.length > 0 && (
+  <div className="d-flex align-items-center mb-2 text-secondary">
+    <span className="fw-semibold">{images.length}</span>&nbsp;
+    <span>{images.length === 1 ? 'image' : 'images'} found</span>
+  </div>
+)}
+
+
+  {images.length>0? (images?.map((item: Image, index: number) => (
+   <div key={index} className="col-12 col-sm-6 col-md-4 position-relative mb-3">
+     <button
+        type="button"
+        className="btn btn-sm position-absolute top-0 end-0 m-2 "
+        title="Delete Image"
+     
+      >
+     <svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="16"
+  height="16"
+  fill="red"
+  className="bi bi-trash"
+  viewBox="0 0 16 16"
+>
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5.5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6zm2.5-.5a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5z"/>
+  <path
+    fillRule="evenodd"
+    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1 0-2H5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1h2.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3a.5.5 0 0 0 0 1H13.5a.5.5 0 0 0 0-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5z"
+  />
+</svg>
+
+      </button>
+ <img
+  src={`${BASE_URL}${item?.path}`}
+  className="rounded shadow-sm object-fit-cover w-100"
+  alt={`${item?.id}`}
+  style={{ height: '300px', objectFit: 'cover' }}
+/>
+
+
+      </div>
+ 
+  ))):(<div className='d-flex align-items-center justify-content-center '><p>No Images Found</p></div>)}
+</div>
 
     </div>
   );
